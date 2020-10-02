ab9d627c24a3ee0b3899f2213552431a78199211
Rohan Raj <rohan.raj@aexp.com>
2020-10-02 14:35:06 +0100

    Module3Exercise - Step 2

--- trello/trello_cards.py
+++ trello/trello_cards.py
@@ -17,3 +17,16 @@ class Item:
 
     def complete(self):
         self.status = 'Done'
+
+    def show_all_done_items(self): 
+        """which will keep track of if we
+        should show all the completed items, or just the most
+        recent ones."""
+
+    def recent_done_items(self): 
+        """which will return all the tasks that
+        have been completed today."""
+    
+    def older_done_items(self): 
+        """which will return all of  the tasks that
+        were completed before today"""

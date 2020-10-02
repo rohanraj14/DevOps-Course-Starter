50318089b8cb5d28e2a6c2508f84a6f1ecf74fee
Rohan Raj <rohan.raj@aexp.com>
2020-10-02 12:20:30 +0100

    Module3Exercise- Step2

--- tests/test_trello_card.py
+++ tests/test_trello_card.py
@@ -9,4 +9,24 @@ def test_create():
 
     #Assert
     assert card.status == 'To Do'
+
+def test_start():
+    #Arrange
+    card = Item('4637436473gehg', 'This is test')
+    
+    #Act
+    card.start()
+
+    #Assert
+    assert card.status == 'Doing'
+
+def test_complete():
+    #Arrange
+    card = Item('4637436473gehg', 'This is test')
+    
+    #Act
+    card.complete()
+
+    #Assert
+    assert card.status == 'Done'
   

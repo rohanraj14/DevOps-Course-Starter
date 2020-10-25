73799e24fee2dfd9dd8d2fb83d39a447e337bcb5
Rohan Raj <rohan.raj@aexp.com>
2020-10-25 07:50:26 +0000

    Module3 Exercise Part2 Step4

--- app.py
+++ app.py
@@ -6,36 +6,35 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('flask_config.Config')
 
+    @app.route('/')
+    def index():
+        """
+        App using Trello API 
+        """
+        items = trello.get_all_cards()
+        item_view_model = ViewModel(items)
+        return render_template('index.html', view_model=item_view_model)
+
+    @app.route('/card/new', methods=['POST'])
+    def add_card():
+        """
+        Adding new Trello card
+        """
+        name = request.form['new_card']
+        trello.add_card_by_name(name)
+        return redirect(url_for('index'))
+
+    @app.route('/card/move/<card_id>')
+    def move_card(card_id):
+        """
+        Moving new Trello card
+        """
+        to_list = request.args.get('to_list')
+        trello.move_card_to_new_list(card_id, to_list)
+        return redirect(url_for('index'))
+    
     return app
 
-app = create_app()
 
-@app.route('/')
-def index():
-    """
-    App using Trello API 
-    """
-    items = trello.get_all_cards()
-    item_view_model = ViewModel(items)
-    return render_template('index.html', view_model=item_view_model)
-
-@app.route('/card/new', methods=['POST'])
-def add_card():
-    """
-    Adding new Trello card
-    """
-    name = request.form['new_card']
-    trello.add_card_by_name(name)
-    return redirect(url_for('index'))
-
-@app.route('/card/move/<card_id>')
-def move_card(card_id):
-    """
-    Moving new Trello card
-    """
-    to_list = request.args.get('to_list')
-    trello.move_card_to_new_list(card_id, to_list)
-    return redirect(url_for('index'))
-
-if __name__ == '__main__':
-    app.run()
+# if __name__ == '__main__':
+#     app.run()
--- tests/test_conftest.py
+++ tests/test_conftest.py
@@ -1,26 +1,21 @@
 import pytest
 import dotenv
-import app
+from app import create_app
 import json
 from trello import trello_service
 from unittest.mock import Mock, patch
-from patch_requests import patch_requests
 
 with open('tests/response_mock.json') as mocklists:
-  data = json.load(mocklists)
-sample_response = json.dumps(data)
+  sample_response = json.load(mocklists)
 
 with open('tests/todo_response_mock.json') as mocktodo:
-  data_todo = json.load(mocktodo)
-sample_response_todo = json.dumps(data_todo)
+  sample_response_todo = json.load(mocktodo)
 
 with open('tests/doing_response_mock.json') as mockdoing:
-  data_doing = json.load(mockdoing)
-sample_response_doing = json.dumps(data_doing)
+  sample_response_doing = json.load(mockdoing)
 
 with open('tests/done_response_mock.json') as mockdone:
-  data_done = json.load(mockdone)
-sample_response_done = json.dumps(data_done)
+  sample_response_done = json.load(mockdone)
 
 @pytest.fixture
 def client():
@@ -28,13 +23,13 @@ def client():
     file_path = dotenv.find_dotenv('.env.test')
     dotenv.load_dotenv(file_path, override=True)
     # Create the new app.
-    test_app = app.create_app()
+    test_app = create_app()
     # Use the app to create a test_client that can be used in our tests.
     with test_app.test_client() as client:
         yield client
         
 
-def mock_get_lists(url, param):
+def mock_get_lists(url, params = {}):
     if url == 'http://api.trello.com/1/boards/5f5a958fa07c17615cc9e37f/lists' :
         mockresponse = Mock()
         mockresponse.json.return_value = sample_response
